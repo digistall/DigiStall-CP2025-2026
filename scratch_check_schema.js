62a1bd6c97bec8bf03d66045144ec16c16121cdb
JenoPro <148298044+JenoPro@users.noreply.github.com>
2026-05-23 22:21:35 +0800

    chore: remove obsolete scratchpad and database maintenance scripts

--- scratch_check_schema.js
+++
@@ -1,8 +0,0 @@
-import { createConnection } from './config/database.js';
-async function run() {
-  const conn = await createConnection();
-  const [rows] = await conn.execute('DESCRIBE stallholder');
-  console.log(rows.find(r => r.Field === 'payment_status'));
-  process.exit(0);
-}
-run();
